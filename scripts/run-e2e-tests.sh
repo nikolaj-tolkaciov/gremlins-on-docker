ba9483c381fa68f519c5b22f4f5684c4ecac76cb
Nikolaj Tolkaciov <nikolajtolkaciov@gmail.com>
2022-07-25 11:37:29 +0300

    fix bug for display error while running test

--- scripts/run-e2e-tests.sh
+++ scripts/run-e2e-tests.sh
@@ -8,8 +8,14 @@ npx playwright install
 
 # Execute gremlins
 echo "Running Playwright tests"
+Xvfb :10 -screen 0 1920x1080x24 2>&1 >/dev/null &
 DISPLAY=:10 npx playwright test
 export RESULT=$?
 
 echo "Playwright tests have finished"
+
+pkill Xvfb
+# Remove temporary folders
+rm -rf .config .local .pki .cache .dbus .gconf .mozilla /tmp/.X10-lock
+
 exit $RESULT
